fbde5211a66939b1fc0409c9331b021b1b49ec67
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-04 23:14:49 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import styles from './App.module.css'
 import { Header } from './components/Header';
-import { NewTask } from './components/NewTask';
 import { TasksList } from './components/TasksList';
 import './global.css';
 
 function App() {
     return (
-    <div className={styles.wrapper}>
+    <div>
       <Header />
-      <NewTask />
       <TasksList />
     </div>
   )
