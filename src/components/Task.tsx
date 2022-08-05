9159273cafb90a21b4f67368da006a2187702e9c
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-04 23:11:37 -0300

    Updated Task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,18 @@
 import styles from "./Task.module.css"
 import trash from "../assets/trash.svg"
 
-export function Task() {
+interface TaskProps {
+    id: string,
+    taskText: string,
+    done: boolean,
+}
+
+export function Task({id, taskText}: TaskProps ) {
     return(
         <div className={styles.task}>
-            <input type="radio"/>
-            <span>oi</span>
+            <input id="inputRadio" type="checkbox"/>
+            <label htmlFor="inputRadio"/>
+            <span>{taskText}</span>
             <button>
                 <img src={trash} alt="Lata de lixo"/>
             </button>
