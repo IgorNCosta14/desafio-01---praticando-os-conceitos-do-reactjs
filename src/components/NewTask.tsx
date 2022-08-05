f64c1d0e182171da85c11b66a01e23b916aa14b9
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-04 23:38:04 -0300

    Update NewTask.tsx

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,14 +1,39 @@
 import { PlusCircle } from "phosphor-react"
+import { ChangeEvent, FormEvent, useState } from "react";
 import styles from "./NewTask.module.css"
 
-export function NewTask() {
+interface newTaskProps {
+    onCreateNewTaskText: () => string;
+}
+
+export function NewTask({onCreateNewTaskText}: newTaskProps) {
+    const [newTaskText, setNewTaskText] = useState('')
+
+    function handleNewTaskText(event: ChangeEvent<HTMLTextAreaElement>) {
+        setNewTaskText(event.target.value);
+    }
+
+    function handleSendNewTaskText(event: FormEvent) {
+        event.preventDefault()
+
+        onCreateNewTaskText(newTaskText)
+
+        console.log(newTaskText);
+
+        setNewTaskText('')
+    }
+
     return (
         <div className={styles.textBox}>
             <textarea 
                 name="comment"
+                value={newTaskText}
+                onChange={handleNewTaskText}
                 placeholder="Adicione uma nova tarefa"
+                
             />
-            <button type="submit">
+
+            <button type="submit" onClick={handleSendNewTaskText}>
                 Criar<PlusCircle/>
             </button>
         </div>
